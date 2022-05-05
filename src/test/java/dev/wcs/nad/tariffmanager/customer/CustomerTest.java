3808153aec26be3f866f4092dd77669cc8b98719
Alexander C <alexander.culum@gmail.com>
2022-05-06 00:41:07 +0200

    Add Tests and Data Import.

--- src/test/java/dev/wcs/nad/tariffmanager/customer/CustomerTest.java
+++ src/test/java/dev/wcs/nad/tariffmanager/customer/CustomerTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import java.io.File;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -18,8 +19,8 @@ public class CustomerTest {
     @Test
     public void shouldCreateDifferentCustomersWithDifferentDiscounts() {
         Customer alfred22 = new VICustomer("1", "Alfred", "alfred@web.de", LocalDate.now().minusYears(22), LocalDate.now().minusDays(100));
-        Customer achim24 = new SpecialCustomer("1", "Alfred", "alfred@web.de", LocalDate.now().minusYears(24), LocalDate.now().minusDays(100));
-        Customer egon54 = new StandardCustomerNoPotential("1", "Alfred", "alfred@web.de", LocalDate.now().minusYears(54), LocalDate.now().minusDays(100));
+        Customer achim24 = new SpecialCustomer("1", "Achim", "achim@web.de", LocalDate.now().minusYears(24), LocalDate.now().minusDays(100));
+        Customer egon54 = new StandardCustomerNoPotential("1", "Egon", "egon@web.de", LocalDate.now().minusYears(54), LocalDate.now().minusDays(100));
 
         assertThat(alfred22.getEmail()).isEqualTo("alfred@web.de");
         assertThat(alfred22).isInstanceOf(VICustomer.class);
@@ -33,19 +34,25 @@ public class CustomerTest {
     @Test
     public void shouldImplementFilterOnGenericPropertiesLikeName() {
         Customer alfred22 = new VICustomer("1", "Alfred", "alfred@web.de", LocalDate.now().minusYears(22), LocalDate.now().minusDays(100));
-        Customer achim24 = new SpecialCustomer("1", "Alfred", "alfred@web.de", LocalDate.now().minusYears(24), LocalDate.now().minusDays(100));
-        Customer egon54 = new StandardCustomerNoPotential("1", "Alfred", "alfred@web.de", LocalDate.now().minusYears(54), LocalDate.now().minusDays(100));
-
-        // filter
+        Customer achim24 = new SpecialCustomer("1", "Achim", "achim@web.de", LocalDate.now().minusYears(24), LocalDate.now().minusDays(100));
+        Customer egon54 = new StandardCustomerNoPotential("1", "Egon", "egon@web.de", LocalDate.now().minusYears(54), LocalDate.now().minusDays(100));
+        List<Customer> customers = List.of(alfred22, achim24, egon54);
+        List<Customer> customersOlderThan22 = customers.stream().filter(customer -> customer.getBirthDate().plusYears(22).isBefore(LocalDate.now())).collect(Collectors.toList());
+        assertThat(customersOlderThan22).hasSize(2);
     }
 
     @Test
     public void shouldImplementLogicOnBaseMethods() {
         Customer alfred22 = new VICustomer("1", "Alfred", "alfred@web.de", LocalDate.now().minusYears(22), LocalDate.now().minusDays(100));
-        Customer achim24 = new SpecialCustomer("1", "Alfred", "alfred@web.de", LocalDate.now().minusYears(24), LocalDate.now().minusDays(100));
-        Customer egon54 = new StandardCustomerNoPotential("1", "Alfred", "alfred@web.de", LocalDate.now().minusYears(54), LocalDate.now().minusDays(100));
-
-        // filter
+        Customer achim24 = new SpecialCustomer("1", "Achim", "achim@web.de", LocalDate.now().minusYears(24), LocalDate.now().minusDays(100));
+        Customer egon54 = new StandardCustomerNoPotential("1", "Egon", "egon@web.de", LocalDate.now().minusYears(54), LocalDate.now().minusDays(100));
+        List<Customer> customers = List.of(alfred22, achim24, egon54);
+        // This collection is filtered using a conventional for-loop
+        for (Customer customer : customers) {
+            if (customer instanceof VICustomer) {
+                System.out.println("Mail of VICustomer: " + customer.getEmail());
+            }
+        }
     }
 
     @Test
