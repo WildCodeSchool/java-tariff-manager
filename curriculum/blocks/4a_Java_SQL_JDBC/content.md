f14c6470ef84b0275d53284a6fae1e87f2ac3301
Alexander C <alexander.culum@gmail.com>
2022-05-19 16:06:03 +0200

    Even more documentation.

--- curriculum/blocks/4a_Java_SQL_JDBC/content.md
+++ curriculum/blocks/4a_Java_SQL_JDBC/content.md
@@ -20,6 +20,28 @@ In this quest you will learn about accessing relational databases with SQL and f
 
 ### Java and JDBC
 
+<details>
+    <summary>Manual JDBC Connection/Statement/ResultSet Handling in Java</summary>
+
+```java
+public Optional<Customer> getByIdJava7Syntax(int id) {
+        // We use try-catch-finally introduced in Java 7
+        try (Connection connection = dataSource.getConnection();
+             // NOTE
+             // For security reasons: Always use PreparedStatements, not Statement
+             PreparedStatement stmt = connection.prepareStatement("my_call");
+             ResultSet resultSet = stmt.executeQuery()) {
+            while (resultSet.next()) {
+                String value = resultSet.getString(1);
+                System.out.println(value);
+            }
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+        return Optional.empty();
+    }
+```
+</details>
 
 ### Challenge: Query Customers with plain JDBC
 
---
+++ src/main/java/dev/wcs/nad/tariffmanager/persistence/jdbc/CustomerLegacyDao.java
@@ -0,0 +1,78 @@
+package dev.wcs.nad.tariffmanager.persistence.jdbc;
+
+import dev.wcs.nad.tariffmanager.customer.model.Customer;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.Optional;
+
+@Component
+public class CustomerLegacyDao {
+
+    private final DataSource dataSource;
+
+    // DataSource is configured by Spring in application.properties and injected during Context setup.
+    public CustomerLegacyDao(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public Optional<Customer> getByIdJava7Syntax(int id) {
+        // We use try-catch-finally introduced in Java 7
+        try (Connection connection = dataSource.getConnection();
+             // NOTE
+             // For security reasons: Always use PreparedStatements, not Statement
+             PreparedStatement stmt = connection.prepareStatement("my_call");
+             ResultSet resultSet = stmt.executeQuery()) {
+            while (resultSet.next()) {
+                String value = resultSet.getString(1);
+                System.out.println(value);
+            }
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+        return Optional.empty();
+    }
+
+    /*
+     * NOTE
+     * This method was included for educational purposes only, please do not use this exception handling any more.
+     * Use the syntax introduced in Java 7 (see above).
+     */
+    public Optional<Customer> getByIdBeforeJava7() {
+        try {
+            Connection connection = dataSource.getConnection();
+            try {
+                CallableStatement callableStatement = connection.prepareCall("my_call");
+                try {
+                    ResultSet resultSet = callableStatement.executeQuery();
+                    try {
+                        while (resultSet.next()) {
+                            String value = resultSet.getString(1);
+                            System.out.println(value);
+                        }
+                    } finally {
+                        try {
+                            resultSet.close();
+                        } catch (Exception ignored) {
+                        }
+                    }
+                } finally {
+                    try {
+                        callableStatement.close();
+                    } catch (Exception ignored) {
+                    }
+                }
+            } finally {
+                try {
+                    connection.close();
+                } catch (Exception ignored) {
+                }
+            }
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+        return Optional.empty();
+    }
+
+}
