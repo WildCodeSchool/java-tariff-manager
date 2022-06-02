09640c9c1b87322e7f4c1d3c94708289b2bf82db
Alexander C <alexander.culum@gmail.com>
2022-06-02 18:08:14 +0200

    Add and Remove classes for Block 5.

--- src/main/java/dev/wcs/nad/tariffmanager/persistence/jdbc/AddressLegacyDao.java
+++ src/main/java/dev/wcs/nad/tariffmanager/persistence/jdbc/AddressLegacyDao.java
@@ -21,39 +21,7 @@ public class AddressLegacyDao {
     }
 
     public Optional<Address> getByIdJava7Syntax(long id) {
-        /*
-        Create the connection object
-        The getConnection() method of DataSource, which is configured by Spring with the configuration of application.properties, is used to establish connection with the database.
-        */
-
-        // We use try-catch-finally introduced in Java 7
-        try (Connection connection = dataSource.getConnection();
-            /*
-             Create the Statement object
-             The preparedStatement() method of Connection interface is used to create statement. The object of statement is responsible to execute queries with the database.
-
-             // NOTE
-             // For security reasons: Always use PreparedStatements, not Statement
-             */
-            PreparedStatement stmt = connection.prepareStatement("SELECT * FROM ADRESS WHERE ID=?")) {
-            stmt.setLong(1, id);
-            /*
-             Execute the query
-             The executeQuery() method of Statement interface is used to execute queries to the database. This method returns the object of ResultSet that can be used to get all the records of a table.
-             */
-            try (ResultSet resultSet = stmt.executeQuery()) {
-                while (resultSet.next()) {
-                    /*
-                     Read results from ResultSet
-                     */
-                    Address address = new Address();
-                    // Map all other Rows here.
-                    return Optional.of(address);
-                }
-            }
-        } catch (SQLException sqlException) {
-            sqlException.printStackTrace();
-        }
+        // Challenge: Add the retrieval of the Address ResultSet and the Mapping to an instance of Address here.
         return Optional.empty();
     }
 
