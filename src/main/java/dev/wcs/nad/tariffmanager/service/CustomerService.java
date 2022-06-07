52d45d2e47613398ca4832012ed57419c81b361f
Alexander C <alexander.culum@gmail.com>
2022-06-07 13:27:33 +0200

    Remove all src in main branch, only leave documentation.

--- src/main/java/dev/wcs/nad/tariffmanager/service/CustomerService.java
+++ src/main/java/dev/wcs/nad/tariffmanager/service/CustomerService.java
@@ -7,7 +7,12 @@ import dev.wcs.nad.tariffmanager.persistence.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
+import java.util.Spliterator;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Service
 public class CustomerService {
@@ -15,13 +20,14 @@ public class CustomerService {
     private final CustomerRepository customerRepository;
     private final AddressRepository addressRepository;
     private final ContactRepository contactRepository;
-    private final int maturityAge;
+    // Challenge: Make legalAge configurable
+    private final int legalAge;
 
-    public CustomerService(CustomerRepository customerRepository, AddressRepository addressRepository, ContactRepository contactRepository, @Value("${maturity.age}") int maturityAge) {
+    public CustomerService(CustomerRepository customerRepository, AddressRepository addressRepository, ContactRepository contactRepository, @Value("${legal.age}") int legalAge) {
         this.customerRepository = customerRepository;
         this.addressRepository = addressRepository;
         this.contactRepository = contactRepository;
-        this.maturityAge = maturityAge;
+        this.legalAge = legalAge;
     }
 
     public Iterable<Customer> readAllCustomers() {
@@ -44,6 +50,33 @@ public class CustomerService {
         customer.setContact(contact);
         return customerRepository.save(customer);
     }
+
+    //After retrieving all customers from the database, create a Stream and filter for all customers aged 18 or older.
+    //In-Memory-Filtering
+    public List<Customer> filterOfLegalAgeCustomersInMemory() {
+        Spliterator<Customer> customerSpliterator = customerRepository.findAll().spliterator();
+        return StreamSupport.stream(customerSpliterator, false).filter(customer -> isOfLegalAge(customer)).collect(Collectors.toList());
+    }
+
+    public Iterable<Customer> filterOfLegalAgeCustomersInRepository() {
+        return customerRepository.findAllByBirthdateIsBefore(LocalDate.now().minusYears(legalAge));
+    }
+
+    public Iterable<Customer> filterOfLegalAgeAndLastname(String lastname) {
+        return customerRepository.findAllByBirthdateIsBeforeAndLastnameContainingIgnoreCase(LocalDate.now().minusYears(legalAge), lastname);
+    }
+
+    private boolean isOfLegalAge(Customer customers) {
+        return customers.getBirthdate().isBefore(LocalDate.now().minusYears(legalAge));
+    }
+
+    //Make sure that a parameter searchFilter can be passed as a QueryParam to further filter the stream for all customers aged >= 18 and name startsWith searchFilter.
+    public Iterable<Customer> filterOfLegalAgeAndName() {
+        return customerRepository.findAllByBirthdateIsBefore(LocalDate.now().minusYears(18));
+    }
+
+
+    //Pass the parameter searchFilter "down" further the layers to optimize performance (not loading all customers, with only the ones containing the searchFilter and age > 18).
 
     public Optional<Customer> findCustomerById(Long id) {
         return customerRepository.findById(id);
