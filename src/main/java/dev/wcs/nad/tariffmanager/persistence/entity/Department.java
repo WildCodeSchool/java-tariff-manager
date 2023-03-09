49063652f03ce19e65cde3cb40508dd2880576de
lgrignon <louis.grignon@protonmail.com>
2023-03-09 15:22:17 +0100

    fix entity for Spring boot 3

--- src/main/java/dev/wcs/nad/tariffmanager/persistence/entity/Department.java
+++ src/main/java/dev/wcs/nad/tariffmanager/persistence/entity/Department.java
@@ -1,11 +1,17 @@
 package dev.wcs.nad.tariffmanager.persistence.entity;
 
-import lombok.Getter;
-import lombok.Setter;
-
-import javax.persistence.*;
 import java.util.LinkedHashSet;
 import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "department")
