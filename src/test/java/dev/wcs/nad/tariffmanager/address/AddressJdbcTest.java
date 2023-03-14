670e38ab215ac5b4e3f7598ea235c277d254cf4c
valentin <valentingueret06@gmail.com>
2023-03-14 14:19:20 +0100

    quest 1and2

--- src/test/java/dev/wcs/nad/tariffmanager/address/AddressJdbcTest.java
+++ src/test/java/dev/wcs/nad/tariffmanager/address/AddressJdbcTest.java
@@ -1,29 +1,41 @@
 package dev.wcs.nad.tariffmanager.address;
 
-import dev.wcs.nad.tariffmanager.customer.model.Customer;
+import dev.wcs.nad.tariffmanager.TariffManagerApplication;
 import dev.wcs.nad.tariffmanager.persistence.entity.Address;
 import dev.wcs.nad.tariffmanager.persistence.jdbc.AddressLegacyDao;
-import dev.wcs.nad.tariffmanager.persistence.jdbc.CustomerLegacyDao;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Import(TariffManagerApplication.class)
 public class AddressJdbcTest {
 
     @Autowired
     private AddressLegacyDao addressLegacyDao;
 
     @Test
-    public void shouldLoadAddressWithId2() {
-        Optional<Address> firstAddress = addressLegacyDao.getByIdJava7Syntax(1);
-        assertThat(firstAddress.isEmpty()).isFalse();
-        assertThat(firstAddress.get().getId().toString().equals("1")).isTrue();
+    public void shouldLoadAddressWithId3() {
+        Optional<Address> secondAddress = addressLegacyDao.getByIdJava7Syntax(3);
+        assertThat(secondAddress.isEmpty()).isFalse();
+        assertThat(secondAddress.get().getId().toString().equals("3")).isTrue();
     }
 
+    @Test
+    public void shouldLoadAddressWithId5() {
+        Optional<Address> address = addressLegacyDao.getByIdJava7Syntax(5);
+        assertThat(address.isEmpty()).isFalse();
+        assertThat(address.get().getId().toString().equals("5")).isTrue();
+    }
 
+    @Test
+    public void shouldReturnEmptyIfAddressNotFound() {
+        Optional<Address> address = addressLegacyDao.getByIdJava7Syntax(123);
+        assertThat(address.isEmpty()).isTrue();
+    }
 }
