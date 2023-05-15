286875356bb250209669f63b8020083e4e7f45f0
lgrignon <louis.grignon@protonmail.com>
2023-05-15 14:38:48 +0200

    fix for tests

--- src/main/java/dev/wcs/nad/tariffmanager/persistence/repository/AddressRepository.java
+++ src/main/java/dev/wcs/nad/tariffmanager/persistence/repository/AddressRepository.java
@@ -1,10 +1,12 @@
 package dev.wcs.nad.tariffmanager.persistence.repository;
 
 import dev.wcs.nad.tariffmanager.persistence.entity.Address;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AddressRepository extends CrudRepository<Address, Long> {
+public interface AddressRepository extends JpaRepository<Address, Long> {
 
 }
